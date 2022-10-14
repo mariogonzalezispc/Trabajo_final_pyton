c3978120de93e7a31b937042463ebf93bc11475e
mario <mario.gonzalez@hotmail.com.ar>
2022-10-13 22:45:15 -0300

    Cabecera

--- Inmobiliaria.py
+++ Inmobiliaria.py
@@ -2,9 +2,21 @@ import pymysql
 try:
 	conexion = pymysql.connect(host='localhost',
                              user='root',
-                             password='',
+                             password='1234',
                              db='inmobiliaria')
 	print("Conexión correcta")
 except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
-	print("Ocurrió un error al conectar: ", e)                           
+	print("Ocurrió un error al conectar: ", e) 
+
+from os import system 
+system("cls")
+
+print("-------------------------------------------------------------------") 
+print("| ISPC TS Telecomunicaciones                                       |")
+print("| Materia: Programacion  Lenguaje : Python                         |")
+print("| Alumno : Mario Gonzalez I                                        |")
+print()
+print()
+print()
+
 
