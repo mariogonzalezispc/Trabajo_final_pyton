76219b88236931672dfa0d1f609dadeee123cc98
Mario <mario.gonzalez@hotmail.com.ar>
2022-11-02 14:35:07 -0300

    Librerias

--- Archivo/Inmobiliaria.py
+++ Archivo/Inmobiliaria.py
@@ -1,12 +1,13 @@
 # Atencion para usar este programa debemos incluir ciertas librerias
 
 #  Para la conexion de la base de datos instalar el conector 
+#  metodo de instalacion
 #  python -m pip install PyMySQL
 #  o
 #  pip install PyMySQL 
 
 #  Colorama para dar color al texto en la consola
-#  metodo de instlacion 
+#  metodo de instalacion 
 #  python -m pip install colorama  
 #  o
 #  pip install colorama
